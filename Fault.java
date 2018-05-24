5157ffcc7960891988723f997b73374dee7e4252
Maricela Rodríguez Núñez <mari.r.n.xd@gmail.com>
2018-05-24 01:29:50 -0500

    Add fault

---
+++ Fault.java
@@ -0,0 +1,78 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.11 
+// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2018.05.23 a las 11:25:11 PM CDT 
+//
+
+
+package com.example.howtodoinjava.schemas.school;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para anonymous complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="ErrorDetail" type="{http://www.citi.com/gcgi/shared/system/fault}ErrorDetail" maxOccurs="unbounded"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "errorDetail"
+})
+@XmlRootElement(name = "Fault", namespace = "http://www.citi.com/gcgi/shared/system/fault")
+public class Fault {
+
+    @XmlElement(name = "ErrorDetail", namespace = "http://www.citi.com/gcgi/shared/system/fault", required = true)
+    protected List<ErrorDetail> errorDetail;
+
+    /**
+     * Gets the value of the errorDetail property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the errorDetail property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getErrorDetail().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link ErrorDetail }
+     * 
+     * 
+     */
+    public List<ErrorDetail> getErrorDetail() {
+        if (errorDetail == null) {
+            errorDetail = new ArrayList<ErrorDetail>();
+        }
+        return this.errorDetail;
+    }
+
+}
